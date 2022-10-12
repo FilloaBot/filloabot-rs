3fe76c46fee3315a7a5b3ce890b815804f87b4a0
peprolinbot <personal@peprolinbot.com>
2022-10-12 13:29:24 +0200

    remove unnecessary code

--- src/utils/chatbot.rs
+++ src/utils/chatbot.rs
@@ -2,23 +2,6 @@ use super::api;
 
 use serenity::model::prelude::*;
 use serenity::prelude::*;
-use serde::{Deserialize, Serialize};
-use regex::Regex;
-
-#[derive(Serialize, Deserialize)]
-struct Reference {
-    #[serde(with = "serde_regex")]
-    regex: Regex,
-    message_reactions: Vec<char>,
-    answer_text: String,
-    answer_image: String,
-    answer_reactions: Vec<char>,
-}
-
-#[derive(Serialize, Deserialize)]
-struct References {
-    references: Vec<Reference>,
-}
 
 pub async fn search_references(ctx: &Context, msg: &Message) -> Result<(), SerenityError> {
     let references = api::references::get(*msg.guild_id.expect("Message wasn't from guild").as_u64()).await;
